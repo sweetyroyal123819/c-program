eafffd8548e4b99c3e2d59185294a387c861daa5
sweetyroyal123819 <lyaswanthiroyal@gmail.com>
2025-12-06 18:34:00 +0530

    factorial.c

---
+++ factorial.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    unsigned long long fact=1;
+    printf("enter an integer:");
+    scanf("%d",&n);
+    if(n<0)
+    printf("error!factorial of a negative number doesn't exist.");
+    else{
+        for(i=0;i<=n;i++){
+            fact*=1;
+        }
+        printf("factorial of %d:%llu,n,fact:");
+    }
+    return 0;
+}
